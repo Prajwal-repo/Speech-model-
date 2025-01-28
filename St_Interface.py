14b0f7629b6931eb01657d6a1bf12babf5d4dc88
Prajwal Mewade <prajwalofficial121@gmail.com>
2025-01-28 22:36:39 +0530

    Gemini Model

--- St_Interface.py
+++ St_Interface.py
@@ -2,15 +2,21 @@ import streamlit as st
 import whisper
 import tempfile
 from llama_model import LlamaModel
+from gemini_model import GeminiModel
 
-st.title("Audio Transcription App")
-st.write("Upload a 1-minute audio file to get the transcription.")
+st.title("Audio Processing App")
+st.write("Upload a 1-minute audio file for processing. ")
 
 uploaded_file = st.file_uploader("Upload an audio file", type=["wav", "mp3"])
 
 model = whisper.load_model("base")
 
-llama = LlamaModel()
+gemini = GeminiModel()
+
+#llama = LlamaModel()
+
+if "transcription_text" not in st.session_state:
+    st.session_state.transcription_text = None
 
 if uploaded_file is not None:
     st.audio(uploaded_file, format="audio/wav")
@@ -23,19 +29,29 @@ if uploaded_file is not None:
     if st.button("Transcribe Audio"):
         try:
             result = model.transcribe(temp_file_path)
-            transcription_text = result["text"]
+            st.session_state.transcription_text = result["text"]
             st.success("Transcription complete")
-            st.markdown(transcription_text)  
+            st.markdown(st.session_state.transcription_text)  
         except Exception as e:
             st.error(f"Error during transcription: {str(e)}")
 
-    if st.button("Generate response"):
+    if st.button("Generate response with Llama Model"):
         try:
             llama_response = llama.generate_response(transcription_text)
             st.success("Llama model response")
             st.markdown(llama_response)
         except Exception as e:
             st.error(f"Error during response generation: {str(e)}")
+    
+    if st.session_state.transcription_text:
+        if st.button("Generate response with Gemini Model"):
+         
+            try:
+                Gemini_response = gemini.generate_response(st.session_state.transcription_text)
+                st.success("Gemini model response")
+                st.markdown(Gemini_response)
+            except Exception as e:
+                st.error(f"Error during response generation: {str(e)}")
 else:
     st.error("Please upload a valid audio file")
 
